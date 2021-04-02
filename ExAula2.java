823b3d74c4f99bc7b333be6b36ea97d983491348
TalyssonG <lopestalysson99@gmail.com>
2021-04-02 14:56:32 -0300

    Ex

---
+++ ExAula2.java
@@ -0,0 +1,20 @@
+
+
+
+package exaula2;
+public class ExAula2 {
+    public static void main(String[] args) {
+        Teclado t1 = new Teclado();
+        t1.cor = "vermelho";
+        t1.modelo = "Redragon";
+        t1.tamanho = 15;
+        
+        t1.desligar();
+        t1.status();
+        t1.digitar();
+        
+        
+               
+    }
+    
+}
